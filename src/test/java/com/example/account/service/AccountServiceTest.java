02dbf6a6b2fcd1e82cd7af04d09631f5818e3041
22rkdmin <22rkdmin@naver.com>
2022-09-23 19:57:42 +0900

    예외처리

---
+++ src/test/java/com/example/account/service/AccountServiceTest.java
@@ -0,0 +1,52 @@
+package com.example.account.service;
+
+import com.example.account.entity.Account;
+import com.example.account.entity.AccountStatus;
+import com.example.account.repository.AccountRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.BDDMockito.given;
+
+@ExtendWith(MockitoExtension.class)
+class AccountServiceTest {
+    @Mock// 가짜를 만들어줌
+    private AccountRepository accountRepository;
+
+    @InjectMocks// 가짜로 만든 repository를 inject해줌
+    private AccountService accountService;
+
+//    @BeforeEach// 테스트 전에 동작하는 메소드
+//    void init(){
+//        accountService.createAccount();
+//    }
+
+    @Test
+    @DisplayName("계좌 조회 성공")// 이름 설정
+    void getAccount(){
+        // given
+        given(accountRepository.findById(anyLong()))
+                .willReturn(Optional.of(Account.builder()
+                        .accountStatus(AccountStatus.UNREGISTERED)
+                        .accountNumber("65789").build()));
+
+        // when
+        Account account = accountService.getAccount(1234L);
+
+        // then
+        assertEquals("65789", account.getAccountNumber());
+        assertEquals(AccountStatus.UNREGISTERED, account.getAccountStatus());
+    }
+
+}

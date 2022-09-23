e1ad8ba0c49d5f9b10d838f261f112960f33d404
22rkdmin <22rkdmin@naver.com>
2022-09-23 16:39:13 +0900

    add redis

--- src/main/java/com/example/account/controller/AccountController.java
+++ src/main/java/com/example/account/controller/AccountController.java
@@ -2,6 +2,7 @@ package com.example.account.controller;
 
 import com.example.account.entity.Account;
 import com.example.account.service.AccountService;
+import com.example.account.service.RedisTestService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,6 +13,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class AccountController {
     private final AccountService accountService;
+    private final RedisTestService redisTestService;
+
+    @GetMapping("/get-lock")
+    public String getLock(){
+        return redisTestService.getLock();
+    }
 
     @GetMapping("/create-account")
     public String createAccount() {
